3cc26dbd0e9f33d2eef8c370eef089553c8b7b78
박지현 <wlgusqkr11@naver.com>
2025-05-25 22:28:02 +0900

    ìfirst commit

--- src/main/java/umc/spring/apiPayload/exception/GeneralException.java
+++ src/main/java/umc/spring/apiPayload/exception/GeneralException.java
@@ -1,21 +1,21 @@
-package umc.spring.apiPayload.exception;
+    package umc.spring.apiPayload.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import umc.spring.apiPayload.ErrorReasonDTO;
-import umc.spring.apiPayload.code.BaseErrorCode;
+    import lombok.AllArgsConstructor;
+    import lombok.Getter;
+    import umc.spring.apiPayload.ErrorReasonDTO;
+    import umc.spring.apiPayload.code.BaseErrorCode;
 
-@Getter
-@AllArgsConstructor
-public class GeneralException extends RuntimeException {
+    @Getter
+    @AllArgsConstructor
+    public class GeneralException extends RuntimeException {
 
-    private BaseErrorCode code;
+        private BaseErrorCode code;
 
-    public ErrorReasonDTO getErrorReason() {
-        return this.code.getReason();
+        public ErrorReasonDTO getErrorReason() {
+            return this.code.getReason();
+        }
+
+        public ErrorReasonDTO getErrorReasonHttpStatus(){
+            return this.code.getReasonHttpStatus();
+        }
     }
-
-    public ErrorReasonDTO getErrorReasonHttpStatus(){
-        return this.code.getReasonHttpStatus();
-    }
-}
